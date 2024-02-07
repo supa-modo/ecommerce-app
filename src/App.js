145bb11937e972ab41bc59e719f148e863daf30c
supa-modo <eddieochieng11@gmail.com>
2024-02-07 13:59:13 +0300

    Added the first component files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
